c3e7ca3a0285245f3b186e9a0ccfa8b0c9b9d0fc
Dessalegne Chekol <dchekol@insp.com>
2019-12-12 12:44:50 -0500

    first commit

---
+++ lblog/resources/views/layouts/footer.blade.php
@@ -0,0 +1,14 @@
+<!-- Footer -->
+<footer class="page-footer font-small blue pt-4">
+
+  <!-- Footer Links -->
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2019 Copyright:
+    <a href="https://insp.com/"> insp.com</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
